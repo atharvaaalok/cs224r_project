ea65952cdb127009dd9b4f9c5f3c4d53e3585ab9
atharvaaalok <atharvaaalok@gmail.com>
2025-05-23 15:25:00 -0700

    Add shape_assets.

--- shape_assets/utils.py
+++ shape_assets/utils.py
@@ -2,6 +2,32 @@ import torch
 import matplotlib.pyplot as plt
 
 
+def automate_training(
+        model,
+        loss_fn,
+        X_train: torch.Tensor,
+        Y_train: torch.Tensor,
+        epochs: int = 1000,
+        print_cost_every: int = 200,
+        learning_rate: float = 0.001,
+) -> None:
+    optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor = 0.99)
+
+    for epoch in range(epochs):
+        Y_model = model(X_train)
+        loss = loss_fn(Y_model, Y_train)
+
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        scheduler.step(loss.item())
+
+        if epoch == 0 or (epoch + 1) % print_cost_every == 0:
+            num_digits = len(str(epochs))
+            print(f'Epoch: [{epoch + 1:{num_digits}}/{epochs}]. Loss: {loss.item():11.6f}')
+
+
 def plot_curves(Xc: torch.Tensor, Xt: torch.Tensor) -> None:
     # Get torch tensor to cpu and disable gradient tracking to plot using matplotlib
     Xc = Xc.detach().cpu()
