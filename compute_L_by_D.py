ed31b113d3abcadf4e02ef77399debc96216a0d2
atharvaaalok <atharvaaalok@gmail.com>
2025-05-23 19:53:12 -0700

    Add try-except for robustness against XFoil.

--- compute_L_by_D.py
+++ compute_L_by_D.py
@@ -2,13 +2,13 @@ import numpy as np
 import numpy.typing as npt
 from xfoil import XFoil
 from xfoil.model import Airfoil
-
+import math
 
 # Create global instance of XFoil class - each instance creates a temporary copy of the fortran code
 # therefore creating a new instance in each run is extremely expensive.
 xf = XFoil()
 
-def compute_L_by_D(X: npt.NDArray, Re = 1e6, max_iter = 100, print_xfoil_output = False) -> float:
+def compute_L_by_D(X: npt.NDArray, Re = 1e6, max_iter = 400, print_xfoil_output = False) -> float:
     """Given an airfoil's coordinates compute the L by D ratio.
 
     Args:
@@ -34,9 +34,15 @@ def compute_L_by_D(X: npt.NDArray, Re = 1e6, max_iter = 100, print_xfoil_output 
     # xf.repanel(n_nodes = 250, cte_ratio = 1)
 
     # Calculate aerodynamic coefficients at 0 angle of attack
-    cl, cd, cm, cp = xf.a(0)
-
-    # Calculate L by D ratio
-    L_by_D = cl / cd
+    try:
+        cl, cd, cm, cp = xf.a(0)
+        # Calculate L by D ratio
+        L_by_D = cl / cd
+    except:
+        # Return
+        L_by_D = None
+    
+    if math.isnan(L_by_D):
+        L_by_D = None
 
     return L_by_D
